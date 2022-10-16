ca5d2a05d4370fd04e8d81baa870047913a5cef1
rankans <rankansen87@gmail.com>
2022-10-16 23:55:37 +0530

    Theme Commit

--- script/app.js
+++ script/app.js
@@ -3,16 +3,21 @@ var jq = $.noConflict();
 var hourDiv = jq("#hour");
 var minuteDiv = jq("#minute");
 var ap = jq("#ap")
+var iconTheme = jq(".icon-theme")
+var themeModal = jq(".theme-modal")
+var mb = jq("#theme1")
+var fl = jq("#theme2")
 
-
-
+iconTheme.on("click", (e)=>{
+    themeModal.removeClass("d-none")
+});
 
 function showTime(){
     var dt = new Date();
     var hour = String(dt.getHours()-12).padStart(2, '0')
     var minute = String(dt.getMinutes()).padStart(2, '0')
 
-    console.log("TIME UPDATED")
+    // console.log("TIME UPDATED")
     jq(document).ready((e) =>{
         hourDiv.text(hour)
         minuteDiv.text(minute)
@@ -22,7 +27,21 @@ function showTime(){
             ap.text("AM")
         }
     })
-    console.log(dt)
+    // console.log(dt)
 }
 
 setInterval(showTime, 1000)
+
+mb.click((e)=>{
+    console.log("THEME CHANGED")
+    jq("body").removeClass()
+    jq("body").addClass("minimal-black")
+    themeModal.addClass("d-none")
+})
+
+fl.click((e)=>{
+    console.log("THEME CHANGED")
+    jq("body").removeClass()
+    jq("body").addClass("floral")
+    themeModal.addClass("d-none")
+})
